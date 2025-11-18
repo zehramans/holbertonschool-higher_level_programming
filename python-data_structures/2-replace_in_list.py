2539153e21c928c4e1dc8ade66926fc9447c80c4
zehramans <zehramansirova0@gmail.com>
2025-11-18 04:24:17 -0500

    changes saved

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,8 +2,7 @@
 
 def element_at(my_list, idx, new_element):
     if idx < 0 or idx >= len(my_list):
-        return my_list[:]
+        return my_list
     else:
-        new_list = my_list[:]
-        new_list[idx] = new_element
+        my_list[idx] = new_element
         return new_list
