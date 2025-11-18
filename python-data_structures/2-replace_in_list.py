f1f2e2f68a8acda23a830bd5d041dbc2e1f3a1cc
zehramans <zehramansirova0@gmail.com>
2025-11-18 04:22:16 -0500

    changes saved

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,8 +2,8 @@
 
 def element_at(my_list, idx, new_element):
     if idx < 0 or idx >= len(my_list):
-        return my_list
+        return my_list[:]
     else:
-        new_list = my_list
+        new_list = my_list[:]
         new_list[idx] = new_element
         return new_list
