e902d9b677cd6303b3d667bf24e60fe05e25094b
zehramans <zehramansirova0@gmail.com>
2025-11-20 14:09:37 +0400

    s

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
-        return ""
+        first_char = ""
     else:
         first_char = sentence[0]
     return length, first_char
