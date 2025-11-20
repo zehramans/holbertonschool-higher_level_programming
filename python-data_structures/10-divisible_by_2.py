f487bd005e9f7750e90aadf5ada957048ff50caf
zehramans <zehramansirova0@gmail.com>
2025-11-20 14:54:35 +0400

    s

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
+    result = []
     for i in my_list:
-        if i % 2 == 0:
-            return True
-        else:
-            return False
+        result.append(i % 2 == 0)
+    return result
+
