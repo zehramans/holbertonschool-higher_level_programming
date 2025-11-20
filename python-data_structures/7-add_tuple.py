88d5ddf42692d4b9bdbc9ce6cb1bcf91659ec4e6
zehramans <zehramansirova0@gmail.com>
2025-11-20 13:57:39 +0400

    s

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) > len(tuple_b):
-        for i in range (len(tuple_a)-len(tuple_b)):
-            tuple_b+=(0, )
-    else:
-        for i in range (len(tuple_b)-len(tuple_a)):
-            tuple_a+=(0, )
-    return tuple(sum(pair) for pair in zip((tuple_a), (tuple_b)))
+    return tuple(sum(pair) for pair in zip((tuple_a + (0, 0)[:2]), (tuple_b + (0, 0))))
 
