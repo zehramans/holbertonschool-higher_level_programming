3d1a19c32bbbd390acb6a214b84e8a9b72be347c
zehramans <zehramansirova0@gmail.com>
2025-11-21 01:38:03 -0500

    changes saved

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    upd = {key: value}
-    a_dictionary.update(upd)
-    for i, j in a_dictionary.items():
-        print("{}: {}".format(i, j))
+    a_dictionary[key] = value
+    return a_dictionary
