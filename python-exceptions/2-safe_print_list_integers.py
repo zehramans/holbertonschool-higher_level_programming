6a04f4d2fa979300c9430d21654d62c479bb93bc
zehramans <zehramansirova0@gmail.com>
2025-11-21 07:11:06 -0500

    changes saved

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     final = 0
     try:
         for i in range(x):
             if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i], end=""))
+                print("{:d}".format(my_list[i]), end="")
                 final += 1
     except IndexError:
-        pass
+        raise IndexError
     finally:
         print()
     return final
