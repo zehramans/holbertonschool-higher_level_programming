bd08745c3ecfdcd233d25510078358fc5fc4305d
zehramans <zehramansirova0@gmail.com>
2025-11-21 09:17:55 -0500

    changes saved

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    final_list = []
+    for i in range(list_length):
+        try:
+            final_list.append(my_list_1[i]/my_list_2[i])
+        
+        except ZeroDivisionError:
+            print("division by 0")
+            final_list.append(0)
+        except TypeError:
+            print("wrong type")
+            final_list.append(0)
+        except IndexError:
+            print("out of range")
+            final_list.append(0)
+    return final_list
