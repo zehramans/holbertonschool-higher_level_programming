06ac798aaeeb6141cc9cb98b206ba9d007c18fd6
zehramans <zehramansirova0@gmail.com>
2025-11-25 17:47:21 -0500

    changes saved

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -8,15 +8,18 @@ class Rectangle(BaseGeometry):
     """rectangle"""
 
     def __init__(self, width, height):
+        """bura da document """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
+        """area calculator da """
         return self.__width * self.__height
 
     def __str__(self):
+        """burda da deyirem ki classin adini verir ozunu cagirma"""
         name = str(self.__class__.__name__)
         a = str(self.__width)
         b = str(self.__height)
