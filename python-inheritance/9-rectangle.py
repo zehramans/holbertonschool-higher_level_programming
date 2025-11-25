542d9361ecfedca0b6d0cf70df6805e67da3d52d
zehramans <zehramansirova0@gmail.com>
2025-11-25 17:44:22 -0500

    changes saved

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+GNU nano 8.6                                       8 - rectangle.py
+#!/usr/bin/python3
+"""rectangle to inherit from basegeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """rectangle"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        name = str(self.__class__.__name__)
+        a = str(self.__width)
+        b = str(self.__height)
+        return f"[{name}] {w}/{h}"
