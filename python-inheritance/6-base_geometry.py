6a04ac82a294d6f5ea52173a013428c4257fcf71
zehramans <zehramansirova0@gmail.com>
2025-11-25 08:56:08 -0500

    changes saved

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+basegeometry
+"""
+
+
+class BaseGeometry:
+    """geometry"""
+
+    def area(self):
+        """public instance"""
+        raise Exception("area() is not implemented")
