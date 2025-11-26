752e246527251d2c8425a3fb28f068348aa6bfce
zehramans <zehramansirova0@gmail.com>
2025-11-25 20:18:37 -0500

    changes saved

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""objecti deyir text fileina yaz deyir amma json kimi deyir"""
+
+
+def save_to_json_file(my_obj, filename):
+    """indi serialize elemek ucun dumps() istifade edecik bir de with open bayaqki"""
+    import json
+    with open(filename, "w", encoding="utf-8")as f:
+        json.dump(my_obj, f)
+        """dumps yox dump olurmus in this case"""
