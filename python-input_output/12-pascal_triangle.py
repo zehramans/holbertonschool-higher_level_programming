6685b84480b9a96a6307b175174db8e3f2e7feaf
zehramans <zehramansirova0@gmail.com>
2025-11-26 03:39:03 -0500

    changes saved

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,22 +3,23 @@
 
 
 def factorial(n):
+    """Return factorial of n (simple iterative version)"""
+    if n == 0:
+        return 1
     f = 1
     for i in range(1, n + 1):
         f *= i
     return f
-    """slaam"""
 
 
 def pascal_triangle(n):
     """salam"""
     if n <= 0:
         return []
-    lists = []
     esaslist = []
     for i in range(n):
+        lists = []
         for j in range(i + 1):
             lists.append(factorial(i) // (factorial(j) * factorial(i - j)))
         esaslist.append(lists)
-        print(lists)
-        lists = []
+    return esaslist
