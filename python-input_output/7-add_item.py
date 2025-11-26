2fe43ea3e9755c22db7db4f4c455139f3c5fba08
zehramans <zehramansirova0@gmail.com>
2025-11-25 20:43:13 -0500

    changes saved

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """elementleri python listine ordan filea"""
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
-
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 """deyrem ki ya existing listi ac yoxdusa yarat"""
 try:
