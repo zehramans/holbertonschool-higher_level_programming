76560397216089ccadd1427e4836ad692ae39ebd
zehramans <zehramansirova0@gmail.com>
2025-11-26 03:27:45 -0500

    changes saved

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""salam"""
+
+
 def factorial(n):
     f = 1
     for i in range(1, n + 1):
         f *= i
     return f
+    """slaam"""
 
 
 def pascal_triangle(n):
+    """salam"""
     if n <= 0:
         return []
     lists = []
