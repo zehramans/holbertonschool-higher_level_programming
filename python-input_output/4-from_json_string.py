5b6046af0535f0f062ced9a3bd222c14b77110c9
zehramans <zehramansirova0@gmail.com>
2025-11-25 20:11:41 -0500

    changes saved

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""onject return elesin amma json str olsun"""
+
+
+def from_json_string(my_str):
+    """dumps() = python objectini serialize eliyir json stringine
+    loads() = json stringini deserialize eliyir python objectine
+    """
+    import json
+    json.loads(my_str)
