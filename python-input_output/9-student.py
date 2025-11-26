022e71f882b07c5a9a8d82dc79d012c6ba611a31
zehramans <zehramansirova0@gmail.com>
2025-11-26 01:52:36 -0500

    changes saved

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,7 +4,8 @@
 
 class Student:
     """public instance attributes"""
-    def __init__(first_name, last_name, age):
+
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
