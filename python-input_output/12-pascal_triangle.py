11980b55faad81afa660ed6e72c87c7f457e1855
zehramans <zehramansirova0@gmail.com>
2025-11-26 03:15:06 -0500

    changes saved

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def factorial(n):
+    f = 1
+    for i in range(1, n + 1):
+        f *= i
+    return f
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    lists = []
+    esaslist = []
+    for i in range(n):
+        for j in range(i + 1):
+            lists.append(factorial(i) // (factorial(j) * factorial(i - j)))
+        esaslist.append(lists)
+        lists = []
+    return esaslist
+
+
+print(pascal_triangle(5))
