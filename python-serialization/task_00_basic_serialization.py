c39e76b5f2b88a0a079c5294ece896b14102d9b0
zehramans <zehramansirova0@gmail.com>
2025-11-27 03:41:08 -0500

    changes saved

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""serialize deserialize again"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """serialize"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(json.dumps(data))
+
+
+def load_and_deserialize(filename):
+    """deserialize"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return f.write(json.loads(filename))
