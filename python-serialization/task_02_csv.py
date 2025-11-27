4d4cebf451f85a0d6e4d805bd214867b9436f92e
zehramans <zehramansirova0@gmail.com>
2025-11-27 09:18:53 -0500

    changes saved

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""qeseng kod"""
+import csv 
+import json
+def convert_csv_to_json(csvfilename):
+    """0 komek tekce ozum yazdim"""
+    try:
+        with open(csvfilename, "r") as csv:
+            reader = list(csv.DictReader(csvfilename))
+        with open("data.json", "w") as f:
+            json.dump(reader, f)
+    except FileNotFoundError:
+        return False
+
