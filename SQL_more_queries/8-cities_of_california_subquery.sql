7e5ea07ff8c91db5fb671d142fc80cc9bdcf2038
zehramans <zehramansirova0@gmail.com>
2025-11-29 17:39:31 -0500

    changes saved

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
---geldi geldiiiieeehh
-SELECT cities.id, cities.name FROM cities, states
-WHERE cities.state_id = states.id
-AND states.name = 'California'
+-- geldi geldiiiieeehh
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id AND states.name = 'California'
 ORDER BY cities.id ASC;
