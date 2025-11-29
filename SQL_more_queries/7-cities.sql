ceb672fdd792e92a471d3498d3fe5e00cc5f6b18
zehramans <zehramansirova0@gmail.com>
2025-11-29 14:47:36 -0500

    changes saved

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,15 +1,9 @@
---salam salam
+-- CReating a complex system
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-
-SET foreign_key_checks = 0;
-
 CREATE TABLE IF NOT EXISTS cities (
-    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
-
-SET foreign_key_checks = 1;
