1141eb5acaafa372ad6768d2667268176796ba7a
zehramans <zehramansirova0@gmail.com>
2025-11-30 03:49:01 -0500

    changes saved

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- join join
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres JOIN tv_show_genres ON tv_genres.id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
