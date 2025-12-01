c7817576f568d9ce950c1f9892cdd6df666e46dd
zehramans <zehramansirova0@gmail.com>
2025-12-01 03:01:22 -0500

    changes saved

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import urllib.request 
+import urllib.request
 import sys
 """to get the argyment passed"""
 
-url = sys.argv[:1]
+url = sys.argv[1]
 
 with urllib.request.urlopen(url) as res:
     a = (res.headers()).get('X-Request-Id')
