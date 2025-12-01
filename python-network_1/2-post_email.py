619ad8ed2e8130b93cf4ce71d6f8a57b27d1d853
zehramans <zehramansirova0@gmail.com>
2025-12-01 04:01:26 -0500

    changes saved

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import urllib.request
+import urllib.parse
+import sys
+"""we need to first parse the data as a header and urlencode it
+then insert it into the response"""
+
+if len(sys.argv) < 2:
+    print()
+
+else:
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
+
+    with urllib.request.urlopen(url, data=data) as res:
+        print(res.read.decode('utf-8'))
