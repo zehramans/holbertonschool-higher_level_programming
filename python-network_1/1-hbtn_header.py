f1b3f887a901e30f52378622ce6cc1791707c134
zehramans <zehramansirova0@gmail.com>
2025-12-01 03:07:48 -0500

    changes saved

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,11 +3,13 @@ import urllib.request
 import sys
 """to get the argyment passed"""
 if len(sys.argv) < 2:
-    return
-"""yene salliyiram duz olmadi"""
-url = sys.argv[1]
+    print()
 
-with urllib.request.urlopen(url) as res:
-    a = (res.headers()).get('X-Request-Id')
-    print(a)
+    """yene salliyiram duz olmadi"""
+else:
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as res:
+        print((res.headers()).get('X-Request-Id'))
     """duz olsa sevinerem salladim biraz"""
+    """bu defe duzdu come on"""
