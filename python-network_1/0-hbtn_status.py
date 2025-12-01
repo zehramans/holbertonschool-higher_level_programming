5ddfbb54ef03a5632c258b6346d2ad38cbfe16ae
zehramans <zehramansirova0@gmail.com>
2025-12-01 02:47:33 -0500

    changes saved

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import urllib
+
+url = "https://intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as res:
+    body = res.read()
+
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf-8')))
