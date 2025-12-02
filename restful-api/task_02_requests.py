1dbba5cabb4dd2096d40e39419614ce1659257cd
zehramans <zehramansirova0@gmail.com>
2025-12-02 06:25:37 -0500

    changes saved

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    resp = requests.get("https://jsonplaceholder.typicode.com/todos")
+    print(resp.status_code())
+    if resp.status_code == 200:
+        js = resp.json()
+        for i in js:
+            print(i['title'])
+
+
+def fetch_and_save_posts():
+    resp = requests.get("https://jsonplaceholder.typicode.com/todos")
+    if resp.status_code == 200:
+        a = []
+        for i in resp.json():
+            dic = {}
+            dic['id'] = i['id']
+            dic['title'] = i['title']
+            dic['body'] = i['completed']
+            a.append(dic)
+        with open('posts.csv', "w") as final_post:
+            writer = csv.DictWriter(
+                final_post, fieldnames=[
+                    'id', 'names', 'body'])
+            writer.writerows(a)
