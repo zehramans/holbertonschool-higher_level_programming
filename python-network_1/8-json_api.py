e8163fb13b7bc0617fdc0028423a66901a971b89
zehramans <zehramansirova0@gmail.com>
2025-12-02 03:38:49 -0500

    changes saved

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if len(sys.argv) < 2:
+    q = ''
+else:
+    q = sys.argv[1]
+    try:
+        response = request.post(
+            "http://0.0.0.0:5000/search_user",
+            data={
+                'q': q})
+        r = response.json()
+    except ValueError:
+        print("Not a valid JSON")
+    if r:
+        print(f"[{r.get('id')}] {r.get('name')}")
+    else:
+        print("No result")
