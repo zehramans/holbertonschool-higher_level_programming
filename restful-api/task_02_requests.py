55f7f43667027bd1305e9cd73e5ed336ae7c83eb
zehramans <zehramansirova0@gmail.com>
2025-12-02 06:31:06 -0500

    changes saved

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -4,7 +4,7 @@ import csv
 
 
 def fetch_and_print_posts():
-    resp = requests.get("https://jsonplaceholder.typicode.com/todos")
+    resp = requests.get("https://jsonplaceholder.typicode.com/posts")
     print(f"Status Code: {resp.status_code}")
     if resp.status_code == 200:
         js = resp.json()
@@ -13,14 +13,14 @@ def fetch_and_print_posts():
 
 
 def fetch_and_save_posts():
-    resp = requests.get("https://jsonplaceholder.typicode.com/todos")
+    resp = requests.get("https://jsonplaceholder.typicode.com/posts")
     if resp.status_code == 200:
         a = []
         for i in resp.json():
             dic = {}
             dic['id'] = i['id']
             dic['title'] = i['title']
-            dic['body'] = i['completed']
+            dic['body'] = i['body']
             a.append(dic)
         with open('posts.csv', "w") as final_post:
             writer = csv.DictWriter(
