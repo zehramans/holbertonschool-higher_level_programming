e84a114c238d053dad7d87cc316b925bc725a75b
zehramans <zehramansirova0@gmail.com>
2025-12-02 02:32:39 -0500

    changes saved

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -2,7 +2,7 @@
 import requests
 """get request"""
 resp = requests.get("https://intranet.hbtn.io/status")
-alo = resp.content()
+alo = resp.content
 print("Body response:")
 print("\t- type: {}".format(type(alo)))
 print("\t- content: {}".format(alo))
